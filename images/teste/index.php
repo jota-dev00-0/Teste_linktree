f5100186c113b184c0e6230cea92969293abbbef
jota-dev00-0 <jpar.dev00@gmail.com>
2024-06-24 00:32:47 -0300

    1.0

---
+++ images/teste/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+echo 'TESTE';
+
+// Isso é um comentário de linha única
+/*
+ Isso é um comentário
+ de múltiplas linhas
+*/
+
+$nome = "Maria";
+$idade = 25;
+echo "Nome: $nome, Idade: $idade";
+
+
+
+$nome = $_POST['nome'];
+echo "Olá, $nome!";
+?>
+
+
+
+
